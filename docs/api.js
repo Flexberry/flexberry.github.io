3ed76e2be4b98a6a450c2c2ab3c66acb00e094f0
nagits <aleksey.nagits@gmail.com>
2015-10-06 12:08:51 +0500

    Initial commit

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ProjectionAttributes"
+    ],
+    "modules": [
+        "ember-flexberry-projections"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-flexberry-projections",
+            "name": "ember-flexberry-projections"
+        }
+    ],
+    "elements": []
+} };
+});
