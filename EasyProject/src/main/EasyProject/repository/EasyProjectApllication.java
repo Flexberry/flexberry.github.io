06220a15bafeae32e87f2ae68571a578a69bd7a3
Vetal18 <102075186+Vetal18@users.noreply.github.com>
2023-11-27 01:00:57 +0200

    Add files via upload

---
+++ EasyProject/src/main/EasyProject/repository/EasyProjectApllication.java
@@ -0,0 +1,13 @@
+package main.EasyProject.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyProjectApplication.class, args);
+    }
+
+}
---
+++ EasyProject/src/main/test/EasyProjectAplicationTests
@@ -0,0 +1,13 @@
+package com.example.EasyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
